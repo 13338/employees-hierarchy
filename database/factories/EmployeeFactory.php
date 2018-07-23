2d2cf7c6db8b6664bfafe1afa8f62cc5131abc7a
Rostik G <g13338@gmail.com>
2018-07-23 17:46:03 +0300

    1.3 & 2.2 employee db fake seed

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+$factory->define(App\Employee::class, function (Faker\Generator $faker) {
+    static $password;
+    $faker = \Faker\Factory::create('ru_RU');
+    return [
+        'director_id' => App\Employee::count() != 0 ? $faker->optional(0.99)->numberBetween(App\Employee::min('id'), App\Employee::max('id')) : null,
+        'fio' => $faker->name,
+        'position' => $faker->jobTitle,
+        'employment_at' => $faker->dateTimeBetween($startDate = '-5 years', $endDate = 'now'),
+        'wages' => $faker->numberBetween(1000, 9000),
+    ];
+});
--- database/seeds/EmployeesTableSeeder.php
+++ database/seeds/EmployeesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class EmployeesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(EmployeesTableSeeder::class);
+    	for ($i=0; $i < 50001; $i++) { 
+    		factory(App\Employee::class, 1)->create();
+    	}
     }
 }
